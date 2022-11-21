ff4b96f76265c22c3015e676f530aafc0ba7f790
Euihyun Lee <lee@euihyun.dev>
2022-11-21 17:01:52 +0900

    feat(libft): ft_bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 14:11:44 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/21 17:01:40 by euihlee          ###   ########.fr       */
+/*   Created: 2022/11/21 16:47:55 by euihlee           #+#    #+#             */
+/*   Updated: 2022/11/21 16:59:29 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n)
+{
+	int	*tmp;
 
-#endif
+	tmp = s;
+	while (n-- > 0)
+		*tmp++ = 0;
+	return ;
+}
