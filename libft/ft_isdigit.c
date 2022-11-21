90f3e7e64605dbd18a69835f79ca826c7413647b
Euihyun Lee <lee@euihyun.dev>
2022-11-21 14:48:33 +0900

    feat(libft): ft_isdigit

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,19 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 14:11:44 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/21 14:48:13 by euihlee          ###   ########.fr       */
+/*   Created: 2022/11/21 13:52:54 by euihlee           #+#    #+#             */
+/*   Updated: 2022/11/21 13:53:23 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
