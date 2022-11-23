86dfb59ff04163a3d819ea53e2a9a5b115c293f5
Euihyun Lee <lee@euihyun.dev>
2022-11-24 00:00:13 +0900

    feat(libft): ft_strrchr

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 23:47:09 by euihlee           #+#    #+#             */
+/*   Updated: 2022/11/23 23:55:42 by euihlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	ch;
+	char	*t;
+
+	ch = (char) c;
+	if (ch == '\0')
+	{
+		while (*s)
+			s++;
+		return ((char *) s);
+	}
+	t = NULL;
+	while (*s)
+	{
+		if (*s == ch)
+			t = (char *) s;
+		s++;
+	}
+	return (t);
+}
