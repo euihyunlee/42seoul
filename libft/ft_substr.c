cc132b97dd0ee96dee47605df7ba940b77b38264
Euihyun Lee <lee@euihyun.dev>
2022-11-24 16:55:44 +0900

    feat(libft): ft_substr

---
+++ libft/ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 14:06:28 by euihlee           #+#    #+#             */
+/*   Updated: 2022/11/24 14:47:36 by euihlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	slen;
+
+	slen = ft_strlen(s);
+	if (start > slen)
+		len = 0;
+	else if (len > slen - start)
+		len = slen - start;
+	substr = malloc(len + 1);
+	if (substr == NULL)
+		return (NULL);
+	substr[len] = '\0';
+	while (len-- > 0)
+		substr[len] = s[start + len];
+	return (substr);
+}
