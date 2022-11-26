51aaf4966f35f6637862a4e612e9814a65fb497e
Euihyun Lee <lee@euihyun.dev>
2022-11-26 17:30:26 +0900

    feat(libft): ft_lstiter

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:11:44 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/26 17:25:33 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/11/26 17:29:51 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
