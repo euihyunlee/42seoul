4e8d5dd1a476d52d5c7c41e5884bb160fee47c66
Euihyun Lee <lee@euihyun.dev>
2022-11-26 17:19:58 +0900

    feat(libft): ft_lstdelone

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:11:44 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/26 17:11:57 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/11/26 17:18:46 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
