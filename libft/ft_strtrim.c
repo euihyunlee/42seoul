8718efd3ebf7cc960b42feaccc84e84979fb1b86
Euihyun Lee <euihlee@student@42seoul.kr>
2022-11-27 17:39:49 +0900

    fix(libft): let ft_strtrim return empty strings

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -21,13 +21,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	s2 = (char *) s1 + ft_strlen(s1);
-	while (s2 > s1 && ft_strchr(set, *s2))
+	while (s2 >= s1 && ft_strchr(set, *s2))
 		s2--;
 	len = s2 - s1 + 1;
 	trimmed = malloc(len + 1);
 	if (trimmed == NULL)
 		return (NULL);
 	trimmed[len] = '\0';
+	if (len == 0)
+		return (trimmed);
 	while (len-- > 0)
 		trimmed[len] = s1[len];
 	return (trimmed);
