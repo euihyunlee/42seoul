9745a1d99b0ad844fba25279c088d922c1a19419
Euihyun Lee <lee@euihyun.dev>
2022-11-29 23:20:08 +0900

    refactor(libft): simplify ft_itoa logic by using buffer array

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,60 +6,36 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 14:27:02 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/28 12:08:57 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/11/29 23:19:08 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	to_unsigned(int n);
-static size_t		get_len(int n);
+#define MAX_DIGITS 22
 
 char	*ft_itoa(int n)
 {
+	char			buffer[MAX_DIGITS];
+	char			*ptr;
 	unsigned int	un;
-	size_t			len;
-	char			*str;
 
-	un = to_unsigned(n);
-	len = get_len(n);
-	str = malloc((len + 1) * sizeof(*str));
-	if (str == NULL)
-		return (NULL);
-	str[len] = '\0';
-	while (len-- > 0)
+	ptr = buffer + MAX_DIGITS - 1;
+	*ptr = '\0';
+	if (n == 0)
 	{
-		str[len] = (un % 10) + '0';
+		*--ptr = '0';
+		return (ft_strdup(ptr));
+	}
+	un = (unsigned int) n;
+	if (n < 0)
+		un = 0 - un;
+	while (un != 0)
+	{
+		*--ptr = (un % 10) + '0';
 		un /= 10;
 	}
 	if (n < 0)
-		str[0] = '-';
-	return (str);
-}
-
-static unsigned int	to_unsigned(int n)
-{
-	if (n < 0)
-		return ((unsigned int) n * -1);
-	return ((unsigned int) n);
-}
-
-static size_t	get_len(int n)
-{
-	size_t			len;
-	unsigned int	un;
-
-	len = 1;
-	un = n;
-	if (n < 0)
-	{
-		un *= -1;
-		len++;
-	}
-	while (un > 9)
-	{
-		un /= 10;
-		len++;
-	}
-	return (len);
+		*--ptr = '-';
+	return (ft_strdup(ptr));
 }
