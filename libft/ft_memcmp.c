ca3b48c1e1a2659dc819fc6e91c53bfcfeb7b060
Euihyun Lee <lee@euihyun.dev>
2022-11-30 00:53:39 +0900

    refactor(libft): reverse return logic in ft_memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -21,12 +21,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		return (0);
 	t1 = (unsigned char *) s1;
 	t2 = (unsigned char *) s2;
-	while (n-- > 0 && *t1 == *t2)
+	while (n-- > 0)
 	{
-		if (n == 0)
-			return (0);
+		if (*t1 != *t2)
+			return (*t1 - *t2);
 		t1++;
 		t2++;
 	}
-	return (*t1 - *t2);
+	return (0);
 }
