b4cea86dfff5825ae1b4f5134f27d780a60a92de
Euihyun Lee <lee@euihyun.dev>
2022-12-05 15:58:41 +0900

    fix(libft): change i from int to unsigned type in ft_striteri

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,13 +6,13 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 15:40:30 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/26 15:43:29 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/12/05 15:57:58 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	int	index;
+	unsigned int	index;
 
 	index = 0;
 	while (s[index])
