e898f83d664b336a5d96ea50f21324209c872575
Euihyun Lee <euihlee@student@42seoul.kr>
2022-12-05 11:15:14 +0900

    fix(libft): handle floating point exception in ft_calloc overflow test

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:32:57 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/24 13:41:33 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/12/05 10:48:52 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	bytes;
 	void	*malloced;
 
-	bytes = count * size;
-	if (bytes / count != size)
+	if (count != 0 && SIZE_MAX / count < size)
 		return (NULL);
-	malloced = malloc(bytes);
+	malloced = malloc(count * size);
 	if (malloced == NULL)
 		return (NULL);
-	return (ft_memset(malloced, 0, bytes));
+	return (ft_memset(malloced, 0, count * size));
 }
