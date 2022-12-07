06b2f4e7b02085797d79c0f61b03278e76dcaf84
Euihyun Lee <euihlee@student@42seoul.kr>
2022-12-07 15:21:33 +0900

    refactor(libft): change return value of ft_memchr from 0 to NULL

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 00:01:02 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/24 00:12:35 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/12/07 15:21:10 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	uc;
 
 	if (n == 0)
-		return (0);
+		return (NULL);
 	t = (unsigned char *) s;
 	uc = (unsigned char) c;
 	while (n-- > 0)
