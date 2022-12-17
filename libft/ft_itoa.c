3d722d36cfb0c60f03eca342de305f618fe3f520
Euihyun Lee <euihlee@student@42seoul.kr>
2022-12-17 19:07:00 +0900

    refactor(libft): revert ft_itoa to older version

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,36 +6,54 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 14:27:02 by euihlee           #+#    #+#             */
-/*   Updated: 2022/11/29 23:19:08 by euihlee          ###   ########.fr       */
+/*   Updated: 2022/12/07 17:42:59 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#define MAX_DIGITS 32
+static size_t		get_len(int n);
 
 char	*ft_itoa(int n)
 {
-	char			buffer[MAX_DIGITS];
-	char			*ptr;
+	size_t			len;
 	unsigned int	un;
+	char			*str;
 
-	ptr = buffer + MAX_DIGITS - 1;
-	*ptr = '\0';
-	if (n == 0)
-	{
-		*--ptr = '0';
-		return (ft_strdup(ptr));
-	}
+	len = get_len(n);
 	un = (unsigned int) n;
 	if (n < 0)
 		un = -un;
-	while (un != 0)
+	str = malloc((len + 1) * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	while (len-- > 0)
 	{
-		*--ptr = (un % 10) + '0';
+		str[len] = (un % 10) + '0';
 		un /= 10;
 	}
 	if (n < 0)
-		*--ptr = '-';
-	return (ft_strdup(ptr));
+		str[0] = '-';
+	return (str);
+}
+
+static size_t	get_len(int n)
+{
+	size_t			len;
+	unsigned int	un;
+
+	len = 1;
+	un = (unsigned int) n;
+	if (n < 0)
+	{
+		un = -un;
+		len++;
+	}
+	while (un > 9)
+	{
+		un /= 10;
+		len++;
+	}
+	return (len);
 }
