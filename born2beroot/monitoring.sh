9b40cabe4870027a3d691903dc3b5d393e8cfdfc
Euihyun Lee <euihlee@student@42seoul.kr>
2023-01-26 21:02:32 +0900

    refactor(b2b): add absolute pathname of lvm and remove spacing in percentages

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -17,12 +17,12 @@ END {
 }')
 echo '# CPU Physical:    ' $socket_count
 echo '# vCPU:            ' $cpu_count
-echo '# CPU Usage:       ' $(mpstat | awk '$3=="all" { printf "%.2f %%", 100 - $13 }')
+echo '# CPU Usage:       ' $(mpstat | awk '$3=="all" { printf "%.2f%%", 100 - $13 }')
 
 # Memory and Disk
 echo '# Memory Usage:    ' $(free --mega | awk '
 /^Mem/ {
-    printf "%d / %d (%.2f %%)", $3, $2, 100 * $3 / $2
+    printf "%d / %d (%.2f%%)", $3, $2, 100 * $3 / $2
 }' | numfmt --from-unit=1M --to=si --field=1,3)
 
 echo '# Disk Usage:      ' $(df --block-size=MB --output=used,size | awk '
@@ -31,10 +31,10 @@ NR > 1 {
     size += $2
 }
 END {
-    printf "%d / %d (%.2f %%)", used, size, 100 * used / size
+    printf "%d / %d (%.2f%%)", used, size, 100 * used / size
 }' | numfmt --from-unit=1M --to=si --field=1,3)
 
-if [[ $(lvm lvscan | grep 'ACTIVE') ]]
+if [[ $(/usr/sbin/lvm lvscan | grep 'ACTIVE') ]]
 then
     lvm_use="Yes"
 else
