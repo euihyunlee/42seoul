6f318077d699794ec2317e51356222086e7852f5
Euihyun Lee <lee@euihyun.dev>
2023-02-19 10:12:01 +0900

    Revert "refactor(push_swap): remove t_bool rev and order variables and add TODOs"
    This reverts commit aa0f63992ec052d71976957f2deb8db32592d50d.

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,36 +6,37 @@
 /*   By: euihlee <euihlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 13:46:04 by euihlee           #+#    #+#             */
-/*   Updated: 2023/02/19 10:03:05 by euihlee          ###   ########.fr       */
+/*   Updated: 2023/02/19 09:45:32 by euihlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_swap(t_dllist *a, t_dllist *b, int size)
+void	push_swap(t_dllist *a, t_dllist *b, int size, t_bool rev)
 {
+	t_bool	order;
 	int		left;
 	int		right;
 
 	if (size < 2)
 		return ;
-	// TODO: optmise sorting for pairs and triples
 	if (size == 2)
 	{
-		if (a->head->n > a->head->next->n)
+		order = a->head->n < a->head->next->n;
+		if ((rev && order) || (!rev && !order))
 			swap(a);
 		return ;
 	}
 	left = size / 2;
 	right = size - left;
-	push_swap(a, b, left);
+	push_swap(a, b, left, rev);
 	rotate(a, left);
-	// TODO: push swap straight to btop
-	push_swap(a, b, right);
+	push_swap(a, b, right, rev);
 	push(a, b, right);
 	while (left && right)
 	{
-		if (b->head->n < a->tail->n)
+		order = b->head->n > a->tail->n;
+		if ((rev && order) || (!rev && !order))
 		{
 			reverse_rotate(a, 1);
 			left--;
